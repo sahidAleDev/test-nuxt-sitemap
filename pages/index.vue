5390b2c9ee14f1820ceef1253b5159e9d2e65279
sahidAle <sahidaledev@gmail.com>
2025-05-27 19:15:14 -0600

    feat(): agregar seo en index

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,14 @@ const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection('content').path('/').first()
 })
 
-
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
